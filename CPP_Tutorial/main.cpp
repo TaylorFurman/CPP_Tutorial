98fa6afd7226739dfafb50d3ed303864e9b0ac00
Taylor Furman <TFurman47@gmail.com>
2022-02-05 12:29:40 -0600

    added variables and did some math

--- CPP_Tutorial/main.cpp
+++ CPP_Tutorial/main.cpp
@@ -1,7 +1,28 @@
+//similar to import in JavaScript
 #include<iostream>
 
+//variable types below (variables written as (type variableName = value)
+
+//int = integers (whole numbers)
+//double = floating point numbers (numbers with decimals)
+//char = stores SINGLE characters, such as 'a' or 'B', must be surrounded by single quotes
+//string = stores text, such as "Hello World!.  must be surrounded by double quotes
+//bool = true/false
+
+//int main() = the opening of the entire program.  The MAIN function from where the program starts.
 int main() 
 {
+	//Print a string
 	std::cout << "Hello World!" << std::endl;
+
+	char theLetterA = 'A';
+	std::cout << theLetterA << std::endl;
+
+	//Just like in JavaScript you can declare variables either inside a function (local) or outside the function (global).
+	int someNumberA = 1;
+	int someNumberB = 2;
+
+	//Print the sum of two numbers
+	std::cout << someNumberA + someNumberB;
 	std::cin.get();
 }
