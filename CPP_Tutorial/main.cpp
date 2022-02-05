0785fa5952382c9fdef0cfb067be5bf53b73ad65
Taylor Furman <TFurman47@gmail.com>
2022-02-05 12:44:38 -0600

    added comments

--- CPP_Tutorial/main.cpp
+++ CPP_Tutorial/main.cpp
@@ -9,6 +9,12 @@
 //string = stores text, such as "Hello World!.  must be surrounded by double quotes
 //bool = true/false
 
+void someFunction() {
+	int sumOfTwo;
+	sumOfTwo = 2 + 2;
+	std::cout << sumOfTwo;
+}
+
 //int main() = the opening of the entire program.  The MAIN function from where the program starts.
 int main() 
 {
@@ -23,6 +29,11 @@ int main()
 	int someNumberB = 2;
 
 	//Print the sum of two numbers
-	std::cout << someNumberA + someNumberB;
+	std::cout << someNumberA + someNumberB << std::endl;
+
+	someFunction();
+
+	//Pauses and waits for user input.  Also used for other things, but non-related to this.
 	std::cin.get();
 }
+
